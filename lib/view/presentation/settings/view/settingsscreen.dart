2fc6384d79549f59ab7d69a2dbad781c81aec44e
MUHAMMED ZAMIL <108522080+MUHAMMEDZAMILC@users.noreply.github.com>
2025-03-25 22:13:38 +0530

    .

--- lib/view/presentation/settings/view/settingsscreen.dart
+++ lib/view/presentation/settings/view/settingsscreen.dart
@@ -25,40 +25,7 @@ class SettingsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ApBtn(
-              onPressed: () async {
-                Products body = Products();
-                body.id = '0016';
-                body.title = "MBJ Women's Solid Short Sleeve Boat Neck V";
-                body.description =
-                    "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem";
-                body.price = 209;
-                body.category = "women's clothing";
-                body.image =
-                    "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg";
-                body.rating = Rating(rate: 4.7, count: 130);
-                await FirebaseFirestore.instance
-                    .collection(productcollection)
-                    .doc(body.id)
-                    .set(body.toJson())
-                    .then((value) {
-                      snackBar(
-                        context,
-                        message: 'Success',
-                        type: MessageType.success,
-                      );
-                    })
-                    .onError((error, stackTrace) {
-                      snackBar(
-                        context,
-                        message: 'Failed',
-                        type: MessageType.error,
-                      );
-                    });
-              },
-              isValid: true,
-              child: AppText(text: 'Add Product', color: ColorResources.WHITE),
-            ),
+            AppText(text: 'Settings Screen')
           ],
         ),
       ),
